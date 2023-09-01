df91f822607551bb9b0d8506206455b259d9fb28
Recep Uyduran <recepuyduran-1@hotmail.com>
2023-09-02 00:52:30 +0300

    Initial Commit

---
+++ task5_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  task5_app
+//
+//  Created by Kasım Sağır on 2.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
