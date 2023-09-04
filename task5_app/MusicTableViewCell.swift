156d15c8dc2c07c0fc86202cb7234d403c7fd75c
Recep Uyduran <recepuyduran-1@hotmail.com>
2023-09-04 19:29:21 +0300

    task5_app Done

---
+++ task5_app/MusicTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MusicTableViewCell.swift
+//  task5_app
+//
+//  Created by Kasım Sağır on 4.09.2023.
+//
+
+import UIKit
+
+class MusicTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
